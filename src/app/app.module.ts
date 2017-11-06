1b0a7fd6403a6f6fc1027e0288f0b7f2b8479991
Ramazan <laumanov.r@gmail.com>
2017-11-06 22:28:20 +0600

    imported http-module, weather-service, router-module and filter-pipe to app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpModule} from '@angular/http';
+import {WeatherService} from "./services/weather.service";
 import { AppComponent } from './app.component';
+import {WeatherListComponent} from "./weather/weather-list.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FloorPipe} from "./filters/floor.pipe";
+
+export const appRoutes: Routes = [
+  {path: '', component: WeatherListComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, WeatherListComponent, FloorPipe],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(appRoutes)],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
